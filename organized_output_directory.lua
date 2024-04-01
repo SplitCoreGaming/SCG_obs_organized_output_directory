3d0729eb7e1ab147a3b42c0e2a3adfe50a991541
Matthew O'Donnell <matthew@splitcoregaming.com>
2024-04-01 12:26:00 +0100

    Update organized_output_directory.lua 1.0.4
    Added multiple features for personal use. 
    Coded in a way that can be shared to others due to failsafes such as 'if additional file exists' and added options to make customising much easier.
    Also uses a dynamic pattern with exemptions to clean all non exempted characters from path. Including any funky spaces or ASCII.

--- organized_output_directory.lua
+++ organized_output_directory.lua
@@ -1,5 +1,5 @@
-local SCRIPT_NAME = "Organized Output Directory"
-local VERSION_STRING = "1.0.1"
+local SCRIPT_NAME = "Organized Output Directory MOD"
+local VERSION_STRING = "1.0.4"
 
 local GITHUB_PROJECT_URL = "https://github.com/MrMartin92/obs_organized_output_directory"
 local GITHUB_PROJECT_LICENCE_URL = "https://raw.githubusercontent.com/MrMartin92/obs_organized_output_directory/main/LICENSE"
@@ -13,13 +13,17 @@ local name_source_enum = {
     ["Process Name"] = 1
 }
 
-local DEFAULT_SCREENSHOT_SUB_DIR = "screenshots"
-local DEFAULT_REPLAY_SUB_DIR = "replays"
+local DEFAULT_SCREENSHOT_SUB_DIR = "Screenshots"
+local DEFAULT_REPLAY_SUB_DIR = "Replays"
 local DEFAULT_NAME_SOURCE = name_source_enum["Window Title"]
+local DEFAULT_additionalfiles_ext = ".txt"
+local DEFAULT_safeallowed_chars = "_'"
 
 local cfg_screenshot_sub_dir
 local cfg_replay_sub_dir
 local cfg_name_source
+local cfg_additionalfiles_ext
+local cfg_safeallowed_chars
 
 local obs = obslua
 
@@ -30,9 +34,10 @@ function script_description()
     To do this, it searches for Window Capture or Game Capture sources in the current scene. \z
     The last active and hooked source is then used to determine the name of the subdirectory from the window title or the process name.<p>\z
     You found a bug or you have a feature request? Great! <a href=\"" .. GITHUB_PROJECT_BUG_TRACKER_URL .. "\">Open an issue on GitHub.</a><p>\z
-    ♥️ If you wish, you can support me on <a href=\"" .. KOFI_URL .. "\">Ko-fi</a>. Thank you! 🤗<p>\z
+    
     <b>🚀 Version:</b> " .. VERSION_STRING .. "<br>\z
-    <b>🧑‍💻 Author:</b> Tobias Lorenz <a href=\"" .. GITHUB_AUTHOR_URL .. "\">[GitHub]</a> <a href=\"" .. TWITCH_AUTHOR_URL .. "\">[Twitch]</a><br>\z
+    <b>🧑‍💻 Author:</b> Tobias Lorenz & SplitCoreGaming <br>\z
+	<b>🧑‍💻 Extended by:</b> SplitCoreGaming <br>\z
     <b>🔬 Source:</b> <a href=\"" .. GITHUB_PROJECT_URL .. "\">GitHub.com</a><br>\z
     <b>🧾 Licence:</b> <a href=\"" .. GITHUB_PROJECT_LICENCE_URL .. "\">MIT</a>"
 end
@@ -42,6 +47,8 @@ function script_properties()
 
     obs.obs_properties_add_text(props, "SCREENSHOT_SUB_DIR", "Screenshot directory name", obs.OBS_TEXT_DEFAULT)
     obs.obs_properties_add_text(props, "REPLAY_SUB_DIR", "Replay directory name", obs.OBS_TEXT_DEFAULT)
+	obs.obs_properties_add_text(props, "additionalfiles_ext", "Addtional File Ext", obs.OBS_TEXT_DEFAULT)
+	obs.obs_properties_add_text(props, "safeallowed_chars", "Allow Non Standard Chars", obs.OBS_TEXT_DEFAULT)	
 
     local props_name_source = obs.obs_properties_add_list(props, "NAME_SOURCE", "Name source", obs.OBS_COMBO_TYPE_LIST, obs.OBS_COMBO_FORMAT_INT)
     for name, value in pairs(name_source_enum) do
@@ -57,6 +64,8 @@ function script_update(settings)
     cfg_screenshot_sub_dir = obs.obs_data_get_string(settings, "SCREENSHOT_SUB_DIR")
     cfg_replay_sub_dir = obs.obs_data_get_string(settings, "REPLAY_SUB_DIR")
     cfg_name_source = obs.obs_data_get_int(settings, "NAME_SOURCE")
+	cfg_additionalfiles_ext = obs.obs_data_get_string(settings, "additionalfiles_ext")
+	cfg_safeallowed_chars = obs.obs_data_get_string(settings, "safeallowed_chars")
 end
 
 function script_defaults(settings)
@@ -65,6 +74,8 @@ function script_defaults(settings)
     obs.obs_data_set_default_string(settings, "SCREENSHOT_SUB_DIR", DEFAULT_SCREENSHOT_SUB_DIR)
     obs.obs_data_set_default_string(settings, "REPLAY_SUB_DIR", DEFAULT_REPLAY_SUB_DIR)
     obs.obs_data_set_default_int(settings, "NAME_SOURCE", DEFAULT_NAME_SOURCE)
+	obs.obs_data_set_default_string(settings, "additionalfiles_ext", DEFAULT_additionalfiles_ext)
+	obs.obs_data_set_default_string(settings, "safeallowed_chars", DEFAULT_safeallowed_chars)
 end
 
 local function get_filename(path)
@@ -129,12 +140,30 @@ end
 
 local function move_file(src, dst)
     print("move_file()")
-    print("\t Src: " .. src)
-    print("\t Dst: " .. dst)
+	
     obs.os_mkdirs(get_base_path(dst))
-    if not obs.os_file_exists(dst) then
-        obs.os_rename(src, dst)
-    else
+	
+	local srcext = src:match("%..+$")
+	print(srcext)
+	print(cfg_additionalfiles_ext)
+	
+	local src = string.gsub(src, srcext, "")
+	print("\t Src: " .. src)
+	
+	local dst = string.gsub(dst, srcext, "")
+	print("\t Dst: " .. dst)
+	
+    if not obs.os_file_exists(dst .. srcext) then
+	
+	    obs.os_rename(src .. srcext, dst .. srcext)
+		
+		if obs.os_file_exists(src .. cfg_additionalfiles_ext) then
+		    if not obs.os_file_exists(dst .. cfg_additionalfiles_ext) then
+	            obs.os_rename(src .. cfg_additionalfiles_ext, dst .. cfg_additionalfiles_ext)
+		    end
+		end
+		
+	else
         print("File aready exist at the destination! So we don't move the file!")
     end
 end
@@ -142,7 +171,8 @@ end
 local function sanitize_path_string(path)
     path = string.gsub(path, "^ +", "") -- Remove leading whitespaces
     path = string.gsub(path, " +$", "") -- Remove trailing whitespaces
-    path = string.gsub(path, "[<>:\\/\"|?*]", "") -- Remove illigal path characters for Windows
+    path = string.gsub(path, "[^%w %+%-%(%)%." .. cfg_safeallowed_chars .. "]", "") -- Remove illegal path characters for Windows
+	-- [™<>:\\/\"|?*] 
     return path
 end
 
@@ -176,11 +206,29 @@ local function replay_event()
     move_file(file_path, new_file_path)
 end
 
+local function record_event()
+    print("record_event()")
+
+    local file_path = obs.obs_frontend_get_last_recording()
+    local game_name = get_game_name()
+
+    if game_name == nil then
+        return
+    end
+
+    local new_file_path = get_base_path(file_path) .. sanitize_path_string(game_name) .. "/" .. get_filename(file_path)
+    
+    move_file(file_path, new_file_path)
+end
+
+
 local function event_dispatch(event)
     if event == obs.OBS_FRONTEND_EVENT_SCREENSHOT_TAKEN then
         screenshot_event()
     elseif event == obs.OBS_FRONTEND_EVENT_REPLAY_BUFFER_SAVED then
         replay_event()
+	elseif event == obs.OBS_FRONTEND_EVENT_RECORDING_STOPPED then
+		record_event()
     end
 end
 
